99cf7b7684ee5296709bf8505c2e13bc90c86a3c
Luisa-Rodriguez <lrodriguezm1991@gmail.com>
2024-03-28 19:42:10 -0500

    mobile-menu

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 const menuEmail= document.querySelector('.navbar-email');
 const desktopMenu= document.querySelector('.desktop-menu');
+const menuResponsive= document.querySelector('.mobile-menu');
+const hamburguerMenu= document.querySelector('.menu');
 
 menuEmail.addEventListener('click',stateDesktopMenu);
+hamburguerMenu.addEventListener('click', stateMenuResponsive);
 
 function stateDesktopMenu () {
 
     desktopMenu.classList.toggle('inactive')
 
+}
+
+function stateMenuResponsive () {
+
+    menuResponsive.classList.toggle('inactive')
 }
 
