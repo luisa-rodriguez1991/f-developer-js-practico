be0caf0c8d2122009e0ed5c04cd30ddb91d7465e
Luisa-Rodriguez <lrodriguezm1991@gmail.com>
2024-03-31 21:23:52 -0500

    menues responsive and destok

--- main.js
+++ main.js
@@ -1,19 +1,41 @@
 const menuEmail= document.querySelector('.navbar-email');
 const desktopMenu= document.querySelector('.desktop-menu');
-const menuResponsive= document.querySelector('.mobile-menu');
+
 const hamburguerMenu= document.querySelector('.menu');
+const menuResponsive= document.querySelector('.mobile-menu');
+
+const shopCar= document.querySelector('.navbar-shopping-cart');
+const shopCarDetail= document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click',stateDesktopMenu);
 hamburguerMenu.addEventListener('click', stateMenuResponsive);
+shopCar.addEventListener('click',stateShopCarDetail);
 
 function stateDesktopMenu () {
 
-    desktopMenu.classList.toggle('inactive')
-
+    const isShopCarclosed = shopCarDetail.classList.contains('inactive');
+    if (!isShopCarclosed) {
+        shopCarDetail.classList.add('inactive');
+    }
+    desktopMenu.classList.toggle('inactive');
+    
 }
 
 function stateMenuResponsive () {
-
-    menuResponsive.classList.toggle('inactive')
+    const isShopCarclosed = shopCarDetail.classList.contains('inactive');
+    if (!isShopCarclosed) {
+        shopCarDetail.classList.add('inactive');
+    }
+    menuResponsive.classList.toggle('inactive');
 }
 
+function stateShopCarDetail() {
+    const isMenuResponsiveclosed = menuResponsive.classList.contains('inactive');
+    const isDesktopMenuclosed = desktopMenu.classList.contains('inactive');
+
+    if (!isMenuResponsiveclosed || !isDesktopMenuclosed ) {
+        menuResponsive.classList.add('inactive');
+        desktopMenu.classList.add('inactive')
+    }
+    shopCarDetail.classList.toggle('inactive')
+}
